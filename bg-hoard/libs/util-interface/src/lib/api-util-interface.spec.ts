87b411a92dcac83e0948cde8fb93eab61c8cd8e8
Rafal Lisowski <rlisowski@slb.com>
2022-09-27 09:22:01 +0100

    lab 13 commit 3

--- bg-hoard/libs/util-interface/src/lib/api-util-interface.spec.ts
+++ bg-hoard/libs/util-interface/src/lib/api-util-interface.spec.ts
@@ -1,7 +1,7 @@
-import { apiUtilInterface } from './api-util-interface';
+// import { apiUtilInterface } from './api-util-interface';
 
-describe('apiUtilInterface', () => {
-  it('should work', () => {
-    expect(apiUtilInterface()).toEqual('api-util-interface');
-  });
-});
+// describe('apiUtilInterface', () => {
+//   it('should work', () => {
+//     expect(apiUtilInterface()).toEqual('api-util-interface');
+//   });
+// });
--- bg-hoard/tools/generators/util-lib/index.ts
+++ bg-hoard/tools/generators/util-lib/index.ts
@@ -1,7 +1,8 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
+import { Schema } from './schema.model';
 
-export default async function (tree: Tree, schema: any) {
+export default async function (tree: Tree, schema: Schema) {
   await libraryGenerator(tree, { 
     name: `util-${schema.name}`,
     directory: schema.directory,
